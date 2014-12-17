b7d84897df1d2e6426b70367165b1e0cd1dca2e9
Romaric <Romaric@Romaric-PC>
2014-12-16 16:11:12 -0800

    Exemple javascript.

--- afficherTexte.js
+++ afficherTexte.js
@@ -4,7 +4,9 @@ $('#listeVideo').on('click', 'a', modifierTexte);
 function modifierTexte() {   
     /*Récuperer text sur l'élément cliquez */
     
-    var recupText = $(this).text();
+    var recupText = $(this).attr("num-episode");
+    
+    alert(recupText);
     
     $("#num_episode").text(recupText);
     
